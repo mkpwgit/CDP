4aba7ffba172ec3b0675bd21eb470b79d3184416
vgerton <191219k1>
2014-02-25 14:24:48 +0300

    remove comments

--- src/main/java/com/epam/cdp/lesson1/part2/DynamicClassOverLoader.java
+++ src/main/java/com/epam/cdp/lesson1/part2/DynamicClassOverLoader.java
@@ -42,13 +42,8 @@ public class DynamicClassOverLoader extends ClassLoader {
         System.out.println("% Class " + name + (f == null ? "" : " found in " + f));
 
         if (f == null) {
+            //call system find class
             return findSystemClass(name);
-            // Обращаемся к системному загрузчику в случае неудачи. findSystemClass – это метод абстрактного класса
-            // ClassLoader с объявлением protected final Class findSystemClass(String name) (т.е. предназначенный
-            // для использования в наследниках и не подлежащий переопределению). Он выполняет поиск и загрузку класса
-            // по алгоритму системного загрузчика. Без вызова findSystemClass(name) нам пришлось бы самостоятельно
-            // позаботиться о загрузке всех стандартных библиотечных классов типа java.lang.String, что потребовало бы
-            // реализовать работу с JAR-архивами (стандартные библиотеки почти всегда упакованы в JAR)
         }
 
         try {
@@ -76,10 +71,6 @@ public class DynamicClassOverLoader extends ClassLoader {
     }
 
     private File findFile(String name, String extension) {
-        // Поиск файла с именем name и, возможно, расширением extension в каталогах поиска, заданных параметром
-        // конструктора classPath. Имена подкаталогов в name разделяются символом '/' – даже если в операционной
-        // системе принят другой разделитель для подкаталогов. (Именно в таком виде получает свой параметр метод
-        // findResource.)
         for (int k=0; k<classPath.length; k++) {
             File f= new File((new File(classPath[k])).getPath()+File.separatorChar+name.replace('/',File.separatorChar)+extension);
             if (f.exists())
@@ -97,9 +88,7 @@ public class DynamicClassOverLoader extends ClassLoader {
             try {
                 f.close();
             } catch (Exception e) {
-                // Игнорируем исключения, возникшие при вызове close. Они крайне маловероятны и не очень
-                // важны - файл уже прочитан. Но если они все же возникнут, то они не должны замаскировать
-                // действительно важные ошибки, возникшие при вызове read.
+
             };
         }
         return result;
